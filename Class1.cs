4b7e3ccd1649cbf101c60d8198e8c93f4fc8b9cb
Jason Clark <Jason.Clark1@niceincontact.com>
2022-06-21 09:16:49 -0400

    Adding changes for 60 target

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class GitDemo
+{
+	public GitDemo()
+	{
+		string add = "Add changes to saved file";
+		string commit = "Commit changes to staging area";
+		string pull = "Pull any repo changes to local branch";
+		string push = "Push commited changes to repo";
+		string merge = "Merge local branch with other branch";
+	}
+}
